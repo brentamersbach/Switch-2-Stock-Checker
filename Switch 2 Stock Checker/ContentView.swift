3bf127d2701d12e0e27d16bf05f974b248a53753
Brent Amersbach <triumshockwave@gmail.com>
2025-06-08 17:04:53 -0400

    Initial Commit

---
+++ Switch 2 Stock Checker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Switch 2 Stock Checker
+//
+//  Created by Brent Amersbach on 6/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Switch 2 Stock Checker/Switch_2_Stock_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Switch_2_Stock_CheckerApp.swift
+//  Switch 2 Stock Checker
+//
+//  Created by Brent Amersbach on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Switch_2_Stock_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
